427906b3ad77a0256f55609f70e1255de4239622
F1ck <51193703+F1ck@users.noreply.github.com>
2019-05-31 07:36:14 +0300

    Add files via upload

--- bot.js
+++ bot.js
@@ -27,7 +27,7 @@ const youtube = new YouTube("AIzaSyAdORXg7UZUo7sePv97JyoDqtQVi3Ll0b8");
 const sql = require("sqlite");
 const dateFormat = require('dateformat'); 
 const pretty = require('pretty-ms') 
-const prefix = '=';
+const prefix = 'a';
 var table = require('table').table
 var ti={}  
 ,spee={}
@@ -35,7 +35,7 @@ var ti={}
 
 client.on('ready', function(){
     var ms = 60000 ;
-    var setGame = ['ahelp','Fuck Off.'];
+    var setGame = ['Fuck Off.','Fuck Off.'];
     var i = -1;
     var j = 0;
     setInterval(function (){
@@ -715,7 +715,7 @@ message.channel.send(
      let embed = new Discord.RichEmbed()
   .setAuthor(message.author.username)
   .setColor("#9B59B6")
-  .addField(" ** :gear: Server Support :gear: **" , "  **https://discord.gg/xA8s2AU**")
+  .addField(" ** :gear: Server Support :gear: **" , "  **https://discord.gg/RWy8P8q**")
      
      
   message.channel.sendEmbed(embed);
